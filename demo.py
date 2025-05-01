d591243bf9b900f2c491e9bd67e2f3a0d9ec7c26
AyoDennis <ayodejidennis33@gmail.com>
2025-05-01 12:50:14 +0100

    update demo.py

--- demo.py
+++ demo.py
@@ -3,12 +3,11 @@ import os
 
 
 name1 = os.environ.get("FIRST_NAME")
-print(name1)
 
 name2 = os.environ.get("LAST_NAME")
 
 name3 = os.environ.get("MIDDLE_NAME")
 
-print(f"I am {name1 } {name2} {name3}")
+print(f"I am {name1} {name2} {name3}")
 
 time.sleep(20000)
